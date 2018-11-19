92e77ed741d0a90db213f88f27f53768888d61d1
Codepuree <Codepuree@bitbucket.org>
2018-11-19 13:25:19 +0100

    Test Measurement

--- src/Take_measurement.m
+++ src/Take_measurement.m
@@ -3,7 +3,7 @@ clear all
 close all
 %% Start measurement
 iteration = 1;
-name = 'Rundgang';
+name = 'kurve';
 folder = ['../Data/' name '/'];
 mkdir(folder);
 
@@ -11,29 +11,38 @@ mkdir(folder);
 colorDevice = imaq.VideoDevice('kinect',1);
 depthDevice = imaq.VideoDevice('kinect',2);
 
-
+disp([datestr(now, 'yyyy-mm-dd HH:MM:SS:FFF') '> Starting measure point cloud mission ''' name '''...']);
 
 while isempty(input(''))
+    disp([datestr(now, 'yyyy-mm-dd HH:MM:SS:FFF') '> Taking measurement ' num2str(iteration) '...']);
     % Capture color & depth image 
     imgColor = step(colorDevice);
     imgDepth = step(depthDevice);
     
     % Point cloud 
     ptCloudAll = pcfromkinect(depthDevice, imgDepth, imgColor);
-    figure('name', 'Unmodified point cloud');
-    pcshow(ptCloudAll, 'VerticalAxis', 'Y', 'VerticalAxisDir', 'down');
-    title('Unmodified point cloud');
-    xlabel('X');
-    ylabel('Y');
-    zlabel('Z');
     
     % Save data
     temp = '';
     if (iteration < 10)
        temp = '0'; 
     end
-    pcwrite(ptCloudAll, [folder name '_' temp num2str(iteration) '.ply']);
+    pcpath = [folder name '_' temp num2str(iteration) '.ply'];
+    pcwrite(ptCloudAll, pcpath);
     
-    disp("take measurement");
+    pc = reshape(ptCloudAll.Location, 480*640, 3);
+    pcnn = pc(~isnan(pc(:, 1)));
+    disp([datestr(now, 'yyyy-mm-dd HH:MM:SS:FFF') '> The point cloud consists of ' num2str(length(pcnn)) ' points.']);
+    
+    % Display point cloud
+    iname = ['Iteration: ' num2str(iteration)];
+    figure();
+    pcshow(ptCloudAll, 'VerticalAxis', 'Y', 'VerticalAxisDir', 'down');
+    title([iname 10 'points: ' num2str(length(pcnn))]);
+    xlabel('X');
+    ylabel('Y');
+    zlabel('Z');
+    
+    disp([datestr(now, 'yyyy-mm-dd HH:MM:SS:FFF') '> Finished writing point cloud number ' num2str(iteration) ' to ''' pcpath '''.']);
     iteration = iteration + 1;
 end
