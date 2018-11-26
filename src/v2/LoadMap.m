fe0be2f17d4a6776287a89b8686a87389c021810
Codepuree <Codepuree@bitbucket.org>
2018-11-26 23:06:09 +0100

    add: notebook binCompare

--- src/v2/LoadMap.m
+++ src/v2/LoadMap.m
@@ -1,9 +1,18 @@
-function [grid] = load_map(filepath)
+function [grid] = load_map(filepath, varargin)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 % load_map('C:\Users\Sysadmin\Downloads\Roessl\Vorgabe_update.png')
+
+p = inputParser();
+
+defaultValResolution = 10;
+validateResolution   = @(x) validateattributes(x, {'single', 'double'}, {'positive'});
+addOptional(p, 'resolution', defaultValResolution, validateResolution);
+
+parse(p, varargin{:});
+
 data_bin = imcomplement(imbinarize(rgb2gray(imread(filepath))));
-%grid = robotics.BinaryOccupancyGrid(data_bin,10);
-grid = robotics.OccupancyGrid(data_bin,10);
+%grid = robotics.BinaryOccupancyGrid(data_bin,p.Results.resolution);
+grid = robotics.OccupancyGrid(data_bin,p.Results.resolution);
 end
 
