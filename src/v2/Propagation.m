72b33238390276f894a63390176d9f60d96df1e8
Nerolf05 <florian.roessl57@gmail.com>
2018-12-22 12:25:42 +0100

    v2delete

--- src/v2/Propagation.m
+++
@@ -1,9 +0,0 @@
-function [particles] = Propagation(map, particles, movement)
-%PROPAGATION Propagates the given particles.
-%   Detailed explanation goes here
-
-[dx, dy] = pol2cart(particles(:,3) + movement(1), movement(2));
-particles(:,1:2) = particles(:,1:2) + [dx dy];
-
-end
-
